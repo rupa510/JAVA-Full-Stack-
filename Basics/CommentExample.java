f96dbd515b643489a65d86dac110860d316fe01b
rupa510 <kollarupasree@gmail.com>
2025-06-19 14:22:04 +0530

    Add files via upload
    Signed-off-by: rupa510 <kollarupasree@gmail.com>

---
+++ Basics/CommentExample.java
@@ -0,0 +1,15 @@
+// This is a single-line comment
+
+/*
+This is a
+multi-line comment
+*/
+
+/**
+ * This is a documentation comment
+ */
+class CommentExample {
+public static void main(String[] args) {
+System.out.println("Comments in Java");
+}
+}
